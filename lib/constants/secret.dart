c514b948f28e08f5b853d5ea6c5af1889c370ee1
Anuj <anujyadav140@gmail.com>
2023-07-07 10:16:00 +0530

    made some state management changes

--- lib/constants/secret.dart
+++ lib/constants/secret.dart
@@ -1,2 +1,2 @@
-const openAIKey = "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";
-// const openAIKey = '';
+// const openAIKey = "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";
+const openAIKey = '';
